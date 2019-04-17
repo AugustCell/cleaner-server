20cddb6d5a69fa1b8ae51694fbf0646888ff24af
Augusto Celis <augusto.celis@stonybrook.edu>
2019-04-17 14:59:11 -0400

    Added sockets.io capabilities.

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-var express = require('express')
+var express = require('express');
+var socket = require('socket.io');
 
 //App setup
 var app = express();
 var server  = app.listen(4000, function(){
-  console.log("Listening to request on port 4000")
+  console.log("Listening to request on port 4000");
 });
 
 //Static files
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+//Socket setup
+var io = socket(server);
+
+io.on('connection', function(socket){
+  console.log("Made socket connection!", socket.id);
+});
